420f9016b6f53cf7b283d5c36ae50085ca6264fd
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-07-13 16:11:32 +0530

    Add Documentation

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -17,64 +17,69 @@ from torch.autograd import Variable
 from model_search import Network
 from architect import Architect
 
-
-parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data',
-                    help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-parser.add_argument('--learning_rate', type=float,
-                    default=0.025, help='init learning rate')
-parser.add_argument('--learning_rate_min', type=float,
-                    default=0.001, help='min learning rate')
-parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
-parser.add_argument('--weight_decay', type=float,
-                    default=3e-4, help='weight decay')
-parser.add_argument('--report_freq', type=float,
-                    default=50, help='report frequency')
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--epochs', type=int, default=50,
-                    help='num of training epochs')
-parser.add_argument('--init_channels', type=int,
-                    default=16, help='num of init channels')
-parser.add_argument('--layers', type=int, default=8,
-                    help='total number of layers')
-parser.add_argument('--model_path', type=str,
-                    default='saved_models', help='path to save the model')
-parser.add_argument('--cutout', action='store_true',
-                    default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int,
-                    default=16, help='cutout length')
-parser.add_argument('--drop_path_prob', type=float,
-                    default=0.3, help='drop path probability')
-parser.add_argument('--save', type=str, default='EXP', help='experiment name')
-parser.add_argument('--seed', type=int, default=2, help='random seed')
-parser.add_argument('--grad_clip', type=float,
-                    default=5, help='gradient clipping')
-parser.add_argument('--train_portion', type=float,
-                    default=0.5, help='portion of training data')
-parser.add_argument('--unrolled', action='store_true',
-                    default=False, help='use one-step unrolled validation loss')
-parser.add_argument('--arch_learning_rate', type=float,
-                    default=3e-4, help='learning rate for arch encoding')
-parser.add_argument('--arch_weight_decay', type=float,
-                    default=1e-3, help='weight decay for arch encoding')
-args = parser.parse_args()
-
-args.save = 'search-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
-utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
-
-log_format = '%(asctime)s %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                    format=log_format, datefmt='%m/%d %I:%M:%S %p')
-fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
-fh.setFormatter(logging.Formatter(log_format))
-logging.getLogger().addHandler(fh)
-
-
 CIFAR_CLASSES = 10
 
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser("cifar")
+    parser.add_argument('--data', type=str, default='../data',
+                        help='location of the data corpus')
+    parser.add_argument('--batch_size', type=int,
+                        default=64, help='batch size')
+    parser.add_argument('--learning_rate', type=float,
+                        default=0.025, help='init learning rate')
+    parser.add_argument('--learning_rate_min', type=float,
+                        default=0.001, help='min learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+    parser.add_argument('--weight_decay', type=float,
+                        default=3e-4, help='weight decay')
+    parser.add_argument('--report_freq', type=float,
+                        default=50, help='report frequency')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+    parser.add_argument('--epochs', type=int, default=50,
+                        help='num of training epochs')
+    parser.add_argument('--init_channels', type=int,
+                        default=16, help='num of init channels')
+    parser.add_argument('--layers', type=int, default=8,
+                        help='total number of layers')
+    parser.add_argument('--model_path', type=str,
+                        default='saved_models', help='path to save the model')
+    parser.add_argument('--cutout', action='store_true',
+                        default=False, help='use cutout')
+    parser.add_argument('--cutout_length', type=int,
+                        default=16, help='cutout length')
+    parser.add_argument('--drop_path_prob', type=float,
+                        default=0.3, help='drop path probability')
+    parser.add_argument('--save', type=str, default='EXP',
+                        help='experiment name')
+    parser.add_argument('--seed', type=int, default=2, help='random seed')
+    parser.add_argument('--grad_clip', type=float,
+                        default=5, help='gradient clipping')
+    parser.add_argument('--train_portion', type=float,
+                        default=0.5, help='portion of training data')
+    parser.add_argument('--unrolled', action='store_true',
+                        default=False, help='use one-step unrolled validation loss')
+    parser.add_argument('--arch_learning_rate', type=float,
+                        default=3e-4, help='learning rate for arch encoding')
+    parser.add_argument('--arch_weight_decay', type=float,
+                        default=1e-3, help='weight decay for arch encoding')
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    args.save = 'search-{}-{}'.format(args.save,
+                                      time.strftime("%Y%m%d-%H%M%S"))
+    utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
+
+    log_format = '%(asctime)s %(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format=log_format, datefmt='%m/%d %I:%M:%S %p')
+    fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+    fh.setFormatter(logging.Formatter(log_format))
+    logging.getLogger().addHandler(fh)
+
     if not torch.cuda.is_available():
         logging.info('no gpu device available')
         sys.exit(1)
@@ -92,6 +97,7 @@ def main():
     criterion = criterion.cuda()
     model = Network(args.init_channels, CIFAR_CLASSES, args.layers, criterion)
     model = model.cuda()
+
     logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
     optimizer = torch.optim.SGD(
@@ -100,6 +106,7 @@ def main():
         momentum=args.momentum,
         weight_decay=args.weight_decay)
 
+    # * Data handling here
     train_transform, valid_transform = utils._data_transforms_cifar10(args)
     train_data = dset.CIFAR10(
         root=args.data, train=True, download=True, transform=train_transform)
@@ -118,6 +125,7 @@ def main():
         sampler=torch.utils.data.sampler.SubsetRandomSampler(
             indices[split:num_train]),
         pin_memory=True, num_workers=2)
+    # * till here
 
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
         optimizer, float(args.epochs), eta_min=args.learning_rate_min)
@@ -148,19 +156,19 @@ def main():
 
 
 def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
-    """[summary]
+    """Trains the network. Gradient step is performed here
 
     Args:
-        train_queue ([type]): [description]
-        valid_queue ([type]): [description]
-        model ([type]): [description]
-        architect ([type]): [description]
-        criterion ([type]): [description]
-        optimizer ([type]): [description]
-        lr ([type]): [description]
+        train_queue (array): Train queue
+        valid_queue (array): Validation queue
+        model (Network): Network
+        architect (Architect): the architechture of network
+        criterion (fn): Loss function
+        optimizer (Optimiser): Adam / SGD
+        lr (float): Learning Rate
 
     Returns:
-        [type]: [description]
+        (float, float): returns top1 avg and overall avg
     """
     objs = utils.AvgrageMeter()
     top1 = utils.AvgrageMeter()
@@ -228,4 +236,5 @@ def infer(valid_queue, model, criterion):
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args)
